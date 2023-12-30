918e02b45a82dbe52995fa0f3d8961b6982e773d
TylerStokkeskog <37637162+TylerStokkeskog@users.noreply.github.com>
2023-12-30 00:28:38 -0400

    Add Tank Flashing when Invincible
    - Tank now flashes when it's invincible
    - Tank material supports transparency (to allow for changing the colour alpha for the flashing)

--- Assets/_Scripts/Player/PlayerVisuals.cs
+++ Assets/_Scripts/Player/PlayerVisuals.cs
@@ -1,14 +1,21 @@
 using Fusion;
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerVisuals : NetworkBehaviour {
     
     // Property name may change based on material shader used. Assuming default is used.
     private const string EMISSION_COLOR_PROPERTY_NAME = "_EmissionColor";
-    
+    private const float FLASH_SPEED = 5f;
+
     [SerializeField] private MeshRenderer[] _meshRenderers;
     [SerializeField] private ParticleSystem _destroyedSFX;
+
+    [Header("Flash when Invincible")]
+    [SerializeField] private PlayerInvincibility _playerInvincibility;
+
+    private Coroutine _invincibleFlashCoroutine;
 
     public ParticleSystem GetDestroyedParticleEffect => _destroyedSFX;
 
@@ -22,6 +29,52 @@ public class PlayerVisuals : NetworkBehaviour {
     public void DestroyedEffect() {
         HidePlayer();
         _destroyedSFX.Play();
+    }
+
+    private void Update() {
+        if (_playerInvincibility == null) {
+            return;
+        }
+
+        if (_invincibleFlashCoroutine != null) {
+            if (_playerInvincibility.IsInvincible == false) {
+                StopInvincibleFlashEffect();
+            }
+            return;
+        }
+
+        if (_playerInvincibility.IsInvincible) {
+            StartInvincibleFlashEffect();
+        }
+    }
+
+    public void StartInvincibleFlashEffect() {
+        _invincibleFlashCoroutine = StartCoroutine(FlashCoroutine());
+    }
+
+    public void StopInvincibleFlashEffect() {
+        StopCoroutine(_invincibleFlashCoroutine);
+        _invincibleFlashCoroutine = null;
+        
+        ForEachMeshRenderer(SetRendererAlpha(1f));
+    }
+
+    private IEnumerator FlashCoroutine() {
+        while (_playerInvincibility.IsInvincible) {
+            float alpha = Mathf.PingPong(Time.time * FLASH_SPEED, 1f);
+            ForEachMeshRenderer(SetRendererAlpha(alpha));
+            yield return null;
+        }
+
+        ForEachMeshRenderer(SetRendererAlpha(1f));
+    }
+
+    private Action<MeshRenderer> SetRendererAlpha(float alphaValue) {
+        return ((MeshRenderer renderer) => {
+            Color changedAlphaColour = renderer.material.color;
+            changedAlphaColour.a = alphaValue;
+            renderer.material.color = changedAlphaColour;
+        });
     }
 
     public void ShowPlayer() => TogglePlayerVisibility(true);
