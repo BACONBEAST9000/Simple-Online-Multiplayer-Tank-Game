f409f9f60a6fcff67a62e0152a97db3d78032dfb
TylerStokkeskog <37637162+TylerStokkeskog@users.noreply.github.com>
2024-02-07 15:29:32 -0400

    Add Game End Screen Timer until Shutdown
    - Timer on end screen. When timer runs out, GameManager calls MSM to Shutdown the session. This is to prevent players from loitering, especially when Photon Free Tier only includes 20 CCPs.

--- Assets/_Scripts/Systems/GameManager.cs
+++ Assets/_Scripts/Systems/GameManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class GameManager : NetworkBehaviour {
 
     [SerializeField] private NetworkTimer _gameTimer;
+    [SerializeField] private NetworkTimer _endScreenTimer;
     [SerializeField] private bool _testingStopGameFromEndng = false;
 
     private void OnEnable() {
         _gameTimer.OnTimerEnd -= WhenGameTimerEnds;
         _gameTimer.OnTimerEnd += WhenGameTimerEnds;
+
+        _endScreenTimer.OnTimerEnd -= WhenEndScreenTimerEnds;
+        _endScreenTimer.OnTimerEnd += WhenEndScreenTimerEnds;
     }
 
     private void OnDisable() {
         _gameTimer.OnTimerEnd -= WhenGameTimerEnds;
+        _endScreenTimer.OnTimerEnd -= WhenEndScreenTimerEnds;
     }
 
     private void WhenGameTimerEnds() {
@@ -24,6 +29,11 @@ public class GameManager : NetworkBehaviour {
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPC_EndGame() {
+        _gameTimer.StopTimer();
         GameStateManager.ChangeState(GameState.GameEnd);
+    }
+    
+    private void WhenEndScreenTimerEnds() {
+        MultiplayerSessionManager.Instance.ShutdownSession();
     }
 }
