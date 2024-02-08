086fa673962ae70ed50b415b2ae12b201185eb8b
TylerStokkeskog <37637162+TylerStokkeskog@users.noreply.github.com>
2024-02-08 15:48:05 -0400

    Refactor Results Entry Text
    - Prefab with Script for Results Player Entry.
    - GameResultsUI has list of ResultsEntryTextUIs
    - GameResultsUI calls ResultsEntryTextUI to update itself using Player (name) and position index.
    - ResultsPositionColours ColourList asset for the Gold, Silver, Bronze, White colouring on the ResultsEntryTextUI.
    - Separating these functionalities makes this project's code cleaner!

--- Assets/GameResultsUI.cs
+++ Assets/GameResultsUI.cs
@@ -1,17 +1,10 @@
 using Fusion;
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GameResultsUI : NetworkBehaviour {
-
-    private static string[] _ordinalNumbers = { "1st", "2nd", "3rd", "4th" };
-    private static float[] _fontSizes = { 72, 64, 56, 42 };
-
     [Header("List of Players")]
-    [SerializeField] private TMP_Text[] _playerPositionTextElements;
-    [SerializeField] private Color[] _positionColours;
+    [SerializeField] private ResultsEntryTextUI[] _orderedPlayerEntries;
 
     private void OnEnable() {
         HideAllTextEntries();
@@ -19,7 +12,7 @@ public class GameResultsUI : NetworkBehaviour {
     }
 
     private void HideAllTextEntries() {
-        foreach (var textElement in _playerPositionTextElements) {
+        foreach (var textElement in _orderedPlayerEntries) {
             textElement.gameObject.SetActive(false);
         }
     }
@@ -32,16 +25,14 @@ public class GameResultsUI : NetworkBehaviour {
 
         for (int i = 0; i < playersOrderedByScore.Count; i++) {
             Player player = playersOrderedByScore[i];
-            if (currentScore != player.Score) {
+
+            bool twoPlayerScoresAreNotTied = currentScore != player.Score;
+            if (twoPlayerScoresAreNotTied) {
                 currentScore = player.Score;
                 positionIndex++;
             }
 
-            // TODO: Refactor
-            _playerPositionTextElements[i].text = $"{_ordinalNumbers[positionIndex]} {player.NickName}";
-            _playerPositionTextElements[i].fontSize = _fontSizes[positionIndex];
-            _playerPositionTextElements[i].color = _positionColours[positionIndex];
-            _playerPositionTextElements[i].gameObject.SetActive(true);
+            _orderedPlayerEntries[i].SetAndDisplayEntry(player, positionIndex);
         }
     }
 }
