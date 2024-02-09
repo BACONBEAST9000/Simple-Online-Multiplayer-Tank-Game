f4f0db675a069a58cf47f6229f3793fde5867ca3
TylerStokkeskog <37637162+TylerStokkeskog@users.noreply.github.com>
2024-02-09 11:46:51 -0400

    Fix Original Material being used/updated
    - Copy of material used instead of original. This prevents the original material from changing with each play session in the editor.
    - TODO: Make sure UI is still readable with current material colour.

--- Assets/_Scripts/MaterialColourInterpolator.cs
+++ Assets/_Scripts/MaterialColourInterpolator.cs
@@ -6,19 +6,28 @@ public class MaterialColourInterpolator : MonoBehaviour {
     // Property name may change based on material shader used. Assuming default is used.
     private const string EMISSION_COLOR_PROPERTY_NAME = "_EmissionColor";
 
-    [SerializeField] private Material _materialToUpdate;
+    [SerializeField] private Material _originalMaterial;
+    [SerializeField] private Renderer[] _renderersToOverrideMaterial;
     [SerializeField] private Color[] _coloursToUse;
     [SerializeField] private float _transitionSeconds = 1f;
+
+    private Material _copyMaterial;
 
     private int _currentColorIndex = 0;
 
     private void Start() {
+        _copyMaterial = new Material(_originalMaterial);
+
+        foreach (Renderer renderer in _renderersToOverrideMaterial) {
+            renderer.material = _copyMaterial;
+        }
+
         StartCoroutine(InterpolateColoursCoroutine());
     }
 
     private IEnumerator InterpolateColoursCoroutine() {
         while (true) {
-            Color startColour = _materialToUpdate.GetColor(EMISSION_COLOR_PROPERTY_NAME);
+            Color startColour = _copyMaterial.GetColor(EMISSION_COLOR_PROPERTY_NAME);
             Color targetColour = _coloursToUse[_currentColorIndex];
             float elapsedTime = 0f;
 
@@ -26,7 +35,7 @@ public class MaterialColourInterpolator : MonoBehaviour {
 
             while (elapsedTime < _transitionSeconds) {
                 colorToSet = Color.Lerp(startColour, targetColour, elapsedTime / _transitionSeconds);
-                _materialToUpdate.SetColor(EMISSION_COLOR_PROPERTY_NAME, colorToSet);
+                _copyMaterial.SetColor(EMISSION_COLOR_PROPERTY_NAME, colorToSet);
 
                 elapsedTime += Time.deltaTime;
                 yield return null;
